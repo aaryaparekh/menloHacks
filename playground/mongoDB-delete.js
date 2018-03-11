9a16602af7c996f4767eac8ea0c37bb7b57cffa7
aarya <aaryaparekh01@gmail.com>
2018-03-10 21:19:42 -0800

    test

--- playground/mongoDB-delete.js
+++ playground/mongoDB-delete.js
@@ -7,9 +7,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
   console.log('Connected to mongodb server');
 
   //deleteMany
-  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
-  //   console.log(result);
-  // });
+  db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
+    console.log(result);
+  });
 
 //deleteOne
 // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,36 +1,29 @@
 //Configuration for moongoose
 const mongoose = require('mongoose');
-
+const {MongoClient, ObjectID} = require('mongodb');
 //Tell mongoose to use promises
 mongoose.Promise = global.Promise;
 
 //connect
 //This part checks if mongodb should use the HEROKU mongodb extension if available, if not just use the local one
 //Essentially if uploaded to heoku, use the heokou mongodb
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/RecommendMe', (err, db)=>{
+MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/RecommendMe', (err, db)=>{
   if(err){
     return console.log("Unable to connect to database server.");
   }
   console.log('Connected to mongodb server');
 
+  setTimeout(function() {
+    db.collection('users').deleteMany({username: "aarya"}).then((result)=>{
+      console.log(result);
+    });
+  }, 1500);
+
   //deleteMany
-  db.collection('Users').deleteMany({completed: false}).then((result)=>{
-    console.log(result);
-  });
-
-//deleteOne
-// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
-//   console.log(result);
-// });
-
-//findOneAndDelete
-//db.collection('Users').findOneAndDelete({completed:false}).then((result)=>{
-//  console.log(result);
-//});
 
 
   //db.close();
-}););
+});
 
 //Export
 module.exports = {
